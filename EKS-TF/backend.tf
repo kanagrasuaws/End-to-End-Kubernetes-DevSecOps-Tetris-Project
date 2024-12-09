a830a48a6ad1621e1c6e8e7d2032076a9c2aa9a4
kanagrasuaws <165900278+kanagrasuaws@users.noreply.github.com>
2024-12-09 17:03:42 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "my-eks-terrform-bucket-v1"
-    region         = "ap-south-1"
+    region         = "us-east-1"
     key            = "Tetris-Project/EKS-TF/terraform.tfstate"
     dynamodb_table = "Lock-Files"
     encrypt        = true
